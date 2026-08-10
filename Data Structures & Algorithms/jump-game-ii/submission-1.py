c4e9868c36317a0adc7ebf4d5e46381b3488a447
Loukas Iatrou <83168523+LoukasIatrou@users.noreply.github.com>
2026-08-10 09:42:39 +0300

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        goal, reach = len(nums)-1 , nums[0]
+        if goal ==  0:
+            return 0
+        l,r = 0,reach
+        count = 1
+        while r < goal:
+            #Iterate through our range of values
+            reach = 0
+            for i in range(l,r+1):
+                reach = max(reach, (i+nums[i]))
+            count +=1 
+            r = l 
+            r = reach 
+        return count 
